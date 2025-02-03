0066bc202d80f9955ff7af3b5478f8c5871b38a3
Don25422 <danglaskimathi5@gmail.com>
2025-02-03 13:24:57 +0300

    first commit

---
+++ introduction.py
@@ -0,0 +1,5 @@
+#Print is a method used to display/output a value
+print("Kim")
+print("eMobilis")
+print(39)
+print(100*20)
---
+++ variable.py
@@ -0,0 +1,16 @@
+#A variable is a container used to store a value
+number = 10
+firstname = "Don"
+lastname = "Kim"
+language = "python"
+a = 10
+b = 15
+
+print(number)
+print(firstname)
+print(lastname)
+print(firstname, lastname)
+print(language, "is a web programing language")
+print(a + b)
+#The sum of 10 and 15 is 25
+print(f"The sum of {a} and {b} is {a +b}")
